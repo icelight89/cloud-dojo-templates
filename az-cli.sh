a43d088e1a1dd1d584a2f7724575edac3d0478d9
dklor <dklor93@gmail.com>
2021-08-25 16:19:24 +0200

    Update CLI script

--- az-cli.sh
+++ az-cli.sh
@@ -8,7 +8,6 @@ SQL_ADMIN_PASSWORD=<sql-admin-password>
 DB=todo-app-sql-db
 STATIC_WEB_APP=todo-app
 GIT_REPO_URL=https://github.com/<github-user>/azure-sql-db-todo-mvc.git
-SUBSCRIPTION_ID=<subscription-id>
 
 # Create Resource Group
 az group create --name $RESOURCE_GROUP --location $LOCATION 
@@ -26,7 +25,7 @@ az sql db create --name $DB --resource-group $RESOURCE_GROUP --server $SQL_SERVE
 az staticwebapp create --name $STATIC_WEB_APP --location $LOCATION --resource-group $RESOURCE_GROUP --source $GIT_REPO_URL --branch main --app-location '/client' --api-location '/api' --login-with-github
 
 # Set function app settings of the Static Web App required to access the Database
-az rest --method put --headers "Content-Type=application/json" --uri "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Web/staticSites/$STATIC_WEB_APP/config/functionappsettings?api-version=2019-12-01-preview" --body @local.settings.properties.json
+az rest --method put --headers "Content-Type=application/json" --uri "/subscriptions/ffd96b6f-1fcb-4bb8-b0ad-b151d795064a/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Web/staticSites/$STATIC_WEB_APP/config/functionappsettings?api-version=2019-12-01-preview" --body @local.settings.properties.json
 
 # Clean up
 #az group delete --name $RESOURCE_GROUP
